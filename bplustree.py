3781e7f120675df4818545a1debfdbf931ca7eaf
adalum <38195538+adalum@users.noreply.github.com>
2023-09-27 00:18:50 +0800

    search added

--- bplustree.py
+++ bplustree.py
@@ -3,30 +3,55 @@ from constant import *
 # Define constants and configurations for the B+ tree
 # BLOCK_SIZE = 400  # Block size in bytes
 NODE_SIZE = BLOCK_SIZE  # Size of a B+ tree node is equal to block size
-# N =  # todo: define N
+
 
 # Create a data structure for a B+ tree node
 class BPlusTreeNode:
-    def __init__(self, is_leaf=True):
+    def __init__(self, pid, is_leaf=True):
         self.keys = []  # List of keys
         self.children = []  # List of indices to records/other TreeNodes
-        # todo: design of indices: must contain index of DatabaseFile.blocks, and index of record within each block
+        self.parent = pid #index of DatabaseFile.blocks parent is stored 
         self.is_leaf = is_leaf  # Flag to indicate whether it's a leaf node
 
 # Create a data structure for the B+ tree
 class BPlusTree:
-    def __init__(self):
+    def __init__(self, database):
         self.root = BPlusTreeNode(is_leaf=True)  # Initialize with a root leaf node
+        self.database = database #reference to DatabaseFile instance
 
     def insert(self, key, value):
         # Insert a key-value pair into the B+ tree
         # Implement insertion algorithm
+        # design of indices: non leaf nodes - store DatabaseFile.block ids of its children nodes  
+        # leaf nodes - store (block_id, record_id) of records 
         pass
 
     def search(self, key):
         # Search for a key in the B+ tree and return associated values
-        # Implement search algorithm
-        pass
+        node = self.root
+        found = False
+        result = []
+
+        while node.is_leaf == False:
+            for i in range(N):
+                if node.keys[i] > key:
+                    loc = self.children[i] #(block id)
+                    node = self.database.blocks[loc]
+                    found=True
+                    break
+            if not found:
+                loc = self.children[N]
+                node = self.database.blocks[loc]
+            found=False
+        #at the leaf node
+        for i in range(N):
+            if node.keys[i]==key:
+                for b_id, r_id in self.children[i]: #(block id, record id)
+                    record = self.database.blocks[b_id][r_id]
+                    result.append(record.serialize())
+                break
+
+        return result
 
     def delete(self, key):
         # Delete the key in the B+ tree and the array that the last layer points to
